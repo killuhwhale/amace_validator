81dd9f108ab333edb68173ed54ff2d67c58e6020
ethancox127 <ethancox16@gmail.com>
2023-07-24 16:31:43 -0700

    scripts update

--- startCROS.sh
+++ startCROS.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-cd chromiumos/src/platform
+cd /home/appval002/chromiumos/src/platform
 cros_sdk
 echo "Run bash startAMACE.sh -d 192.168.1.123 -d 192.168.1.456 -a account@gmail.com:password"
