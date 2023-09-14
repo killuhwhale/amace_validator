7f44adb6e451e8ef5361a490d1c58dd572b7cc89
killuh <20297127+killuhwhale@users.noreply.github.com>
2023-09-13 17:15:06 -0700

    update deploy code

--- updateRemoteDevice.sh
+++ updateRemoteDevice.sh
@@ -1,8 +1,8 @@
 # This file should be placed in wssTriggerEnv/wssTrigger so that the update client can call it
 
-cd /home/appval002/amaceValidator
+cd /home/appval002/amace_validator
 git pull
 bash linkTests.sh
 
-echo "cd /home/appval002/amaceValidator; git pull; bash linkTests.sh;"
+echo "cd /home/appval002/amace_validator; git pull; bash linkTests.sh;"
 
