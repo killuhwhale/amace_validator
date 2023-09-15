a21f9f6343e23dbeeadfe06fb5effa44f78bc7f6
killuh <20297127+killuhwhale@users.noreply.github.com>
2023-09-15 00:01:16 -0700

    testing wssUpdater

--- updateRemoteDevice.sh
+++ updateRemoteDevice.sh
@@ -1,9 +1,11 @@
 # This file should be placed in wssTriggerEnv/wssTrigger so that the update client can call it
-
+lastdir=$(pwd)
+echo "Current at ${lastdir}"
 echo "cd /home/appval002/amace_validator; git pull; bash linkTests.sh;"
 cd /home/appval002/amace_validator
 git pull
 sleep 10000
 bash linkTests.sh
-
+sleep 1000
+cd ${lastdir}
 
