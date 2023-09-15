6e9854112a4dc85367d242b097f84a9ee38cded0
killuh <20297127+killuhwhale@users.noreply.github.com>
2023-09-14 23:51:17 -0700

    testing wssUpdater

--- updateRemoteDevice.sh
+++ updateRemoteDevice.sh
@@ -1,8 +1,8 @@
 # This file should be placed in wssTriggerEnv/wssTrigger so that the update client can call it
 
-cd /home/appval002/amace_validator
-git pull # update source files from repo
-bash linkTests.sh # Place src files in required location on host device.
-
 echo "cd /home/appval002/amace_validator; git pull; bash linkTests.sh;"
+cd /home/appval002/amace_validator
+git pull
+bash linkTests.sh
+
 
