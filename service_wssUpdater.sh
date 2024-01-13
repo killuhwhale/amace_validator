2a1086a554085b1c1fcbeb34dd8417c196e81335
killuh <20297127+killuhwhale@users.noreply.github.com>
2024-01-12 16:53:59 -0800

    Update wssUpdater service file to use system python3

--- service_wssUpdater.sh
+++ service_wssUpdater.sh
@@ -10,7 +10,7 @@ After=network.target
 User=appval002
 Group=www-data
 WorkingDirectory=/home/appval002/chromiumos/src/scripts/wssTriggerEnv/wssTrigger
-ExecStart=/usr/bin/env bash -c '/home/appval002/chromiumos/src/scripts/wssTriggerEnv/bin/python3 /home/appval002/chromiumos/src/scripts/wssTriggerEnv/wssTrigger/wssUpdater.py'
+ExecStart=/usr/bin/env bash -c 'python3 /home/appval002/chromiumos/src/scripts/wssTriggerEnv/wssTrigger/wssUpdater.py'
 Restart=always
 
 [Install]
