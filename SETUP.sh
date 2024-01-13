36158cb1051d2d30197e352ab22314557066a513
killuh <20297127+killuhwhale@users.noreply.github.com>
2024-01-12 20:14:56 -0800

    setup host device

--- SETUP.sh
+++ SETUP.sh
@@ -1,22 +1,24 @@
 # Runs scripts needed to setup automation
 
+CONFIG="./config.json"
+SUDO_PASSWORD=$(jq -r '.SUDO_PASSWORD' "$CONFIG")
 # Copy files from Repo to TAST and WSS clients' directories
 bash linkTests.sh
 
 # Create and start service
-sudo bash service.sh
-sudo systemctl daemon-reload
-sudo systemctl enable imageserver.service
-sudo systemctl start imageserver.service
+echo "$SUDO_PASSWORD" | sudo -S bash service.sh
+echo "$SUDO_PASSWORD" | sudo -S systemctl daemon-reload
+echo "$SUDO_PASSWORD" | sudo -S systemctl enable imageserver.service
+echo "$SUDO_PASSWORD" | sudo -S systemctl start imageserver.service
 
 # Create and start service
-sudo bash service_wssClient.sh
-sudo systemctl daemon-reload
-sudo systemctl enable wssClient.service
-sudo systemctl start wssClient.service
+echo "$SUDO_PASSWORD" | sudo -S sudo bash service_wssClient.sh
+echo "$SUDO_PASSWORD" | sudo -S sudo systemctl daemon-reload
+echo "$SUDO_PASSWORD" | sudo -S sudo systemctl enable wssClient.service
+echo "$SUDO_PASSWORD" | sudo -S sudo systemctl start wssClient.service
 
 # Create and start service
-sudo bash service_wssUpdater.sh
-sudo systemctl daemon-reload
-sudo systemctl enable wssUpdater.service
-sudo systemctl start wssUpdater.service
+echo "$SUDO_PASSWORD" | sudo -S sudo bash service_wssUpdater.sh
+echo "$SUDO_PASSWORD" | sudo -S sudo systemctl daemon-reload
+echo "$SUDO_PASSWORD" | sudo -S sudo systemctl enable wssUpdater.service
+echo "$SUDO_PASSWORD" | sudo -S sudo systemctl start wssUpdater.service
